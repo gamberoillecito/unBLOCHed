b489084a3dc2038fe20ffc1a9402d18b9d0b6cfe
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-04-03 08:45:36 +0200

    fixed issue with libraries and brought working version of MathField

--- bloch_app/src/lib/components/MathField.svelte
+++ bloch_app/src/lib/components/MathField.svelte
@@ -1,28 +1,63 @@
 <script lang="ts">
-	import 'mathlive'
-  import  type {MathfieldElement} from 'mathlive'
-	import { onMount } from 'svelte';
-  let {latex = $bindable()} = $props();
-  let mathfieldElement: MathfieldElement;
-	$effect(() => {
-		mathfieldElement.value = latex;
-	});
-  const updateLatex = () => {
-    latex = mathfieldElement.value;
-  };
+  import "mathlive";
+  import type { MathfieldElement, MathfieldElementAttributes } from "mathlive";
+  import { onMount} from "svelte";
+  import {on} from "svelte/events"
+  import "mathlive/fonts.css"
+  
+  export type promptsDict = {[key: string]:string}
+  type Props = { value?: string, prompts?:promptsDict } & Partial<MathfieldElementAttributes>;
+  let { value = $bindable(), 
+        prompts = $bindable(),
+        getPromptValue = $bindable(),
+        expression = $bindable(),
+        ...rest }: Props = $props();
 
-	  onMount(() => {
-    mathfieldElement.addEventListener('mount', () => {
-      // You can configure the mathfield here
-      // ...
-			mathfieldElement.smartMode = true;
-      
+  prompts = {};
+  const init = (node: MathfieldElement) => {
+    for (let p of node.getPrompts()){
+        console.log(p);
+        prompts[p] = node.getPromptValue(p)
+    }
+    node.macros =  {
+        minutes: "\\,{}^\\prime\\;",
+        seconds: "\\,\\doubleprime\\;",
+    };
+    $effect(() => {
+        if (value) node.value = value;
+        
+        for (let p in prompts) {
+            if (node.getPromptValue(p) != prompts[p]){
+                node.setPromptValue(p, prompts[p], {})
+            }
+        }
+        for (let p of node.getPrompts()) {
+            prompts[p] = node.getPromptValue(p);
+        }
+
     });
-  });
-	
+    $effect(() => {
+      return on(node, "change", () => {
+        value = node.value;
+        for (let p of node.getPrompts()) {
+            prompts[p] = node.getPromptValue(p);
+        }
+      });
+    });
+  };
 </script>
 
-<math-field
-  bind:this={mathfieldElement}
-  oninput={updateLatex}
-></math-field>
+<math-field use:init {...rest}></math-field>
+
+<style>
+    math-field::part(virtual-keyboard-toggle) {
+        display: none;
+    }
+    math-field::part(menu-toggle) {
+        display: none;
+    }
+    math-field::part(placeholder) {
+        border: none;
+        background-color: red;
+    }
+</style>
