a1fce2a9b416a80cb03d9ff1c8821e1a5ae7764e
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-06-02 17:56:33 +0200

    old changes

--- bloch_app/src/lib/components/App.svelte
+++ bloch_app/src/lib/components/App.svelte
@@ -8,49 +8,15 @@
   import MathField from "$lib/components/MathField.svelte"
   import type {promptsDict} from "$lib/components/MathField.svelte"
   import {ComputeEngine} from  "@cortex-js/compute-engine"
-
+  import MatrixField from './MatrixField.svelte';
   let DM_latex =  $state('\\placeholder[mult]{1}\\cdot\\begin{bmatrix}\\placeholder[m00]{1} & \\placeholder[m01]{0}\\\\ \\placeholder[m10]{0} & \\placeholder[m11]{1}\\end{bmatrix}');
 
   let DM = new DensityMatrix();
   let DM_prompts: promptsDict = $state({});
-  // let matrix_gate: ComplexMat2x2 = $state([
-  //       [complex(1), complex(0)], 
-  //       [complex(0), complex(0)]
-  //   ]);
 
   let ce = new ComputeEngine();
   $inspect(DM.mat);
   $inspect(DM_prompts);
-
-  $effect(() => {
-    
-    for (let i =0; i < 2; i++) {
-      for (let j =0; j < 2; j++) {
-        let idx = `m${i}${j}`
-        if (DM_prompts[idx]){
-
-          let expr = ce.parse(DM_prompts[idx]).N();
-          if (expr){
-            let mult = ce.parse(DM_prompts['mult']).N();
-            console.log(`mult: ${mult}`);
-            console.log("expr")
-            console.log(expr.re)
-            DM.mat[i][j] = multiply(complex(expr.re, expr.im), complex(mult.re, mult.im)) as Complex;
-          }
-        }
-      }
-    }
-  })
-
-  // $effect(()=>{console.log("inspect DM:"); print_mat(DM.mat)})
-
-  const mathLiveConfig = {
-    "smart-mode": 'true',
-    "keypressSound": null,
-    "defaultMode": "math",
-    "menuItems": [],
-    "math-virtual-keyboard-policy": "manual"
-  }
 </script>
 
 
@@ -65,7 +31,7 @@
 <div>
 <div>
 Matrix:
-<MathField bind:value={DM_latex}  bind:prompts={DM_prompts} read-only mathlLiveConfig ></MathField>
+<MatrixField bind:matrix={DM.mat}></MatrixField>
 <p>Current LaTeX: {DM_latex}</p>
 <input type="text" bind:value={DM_latex}>
 
