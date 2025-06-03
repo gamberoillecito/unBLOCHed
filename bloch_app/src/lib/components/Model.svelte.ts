9ac0387330e93a75d56b368b7e649c48217f96c8
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-06-03 23:08:06 +0200

    changed location of class definitions

--- bloch_app/src/lib/components/Model.svelte.ts
+++ bloch_app/src/lib/components/Model.svelte.ts
@@ -20,37 +20,48 @@ function dagger(mat:ComplexMat2x2) {
 }
 
 export class DensityMatrix {
-    mat: ComplexMat2x2 = $state([
-        [complex(1), complex(0)], 
-        [complex(0), complex(0)]
-    ]);
-    latexMat: string|null[][];
-    #a = $derived(this.mat[0][0]);
-    #b = $derived(this.mat[0][1]);
-    #c = $derived(this.mat[1][0]);
-    #d = $derived(this.mat[1][1]);
-
-
+    #mat: ComplexMat2x2;
+    latexMat: (string | null)[][];
+    #a: Complex; 
+    #b: Complex; 
+    #c: Complex; 
+    #d: Complex; 
+    #blochV: [number, number, number];
     // Note that the values of y and z are swapped to account
     // for the fact that threejs uses a different notation
     // This *should* allow us to forget about the different
     // notation in the rest of the code
-    #blochV: [number, number, number] = $derived([
-        2*this.#b.re,
-        2*this.#a.re - 1,
-        2*this.#b.im
-    ])
 
     ce: ComputeEngine;
 
     constructor(){
         this.ce = new ComputeEngine();
+        this.#mat= $state([
+            [complex(1), complex(0)], 
+            [complex(0), complex(0)]
+        ]);
         this.latexMat = [[null, null], [null, null]];
+        this.#a = $derived(this.#mat[0][0]);
+        this.#b = $derived(this.#mat[0][1]);
+        this.#c = $derived(this.#mat[1][0]);
+        this.#d = $derived(this.#mat[1][1]);
+
+        this.#blochV= $derived([
+        2*this.#b.re,
+        2*this.#a.re - 1,
+        2*this.#b.im
+    ])
     }
 
-    setLatex(latex:string, DM: DensityMatrix, i: number, j:number) {
+    setValue(value: Complex, i: number, j:number) {
+        this.#mat[i][j] = value;
+        this.latexMat[i][j] = null;
+    }
+    setLatex(latex:string, i: number, j:number) {
         let converted = this.ce.parse(latex).N();
-        DM.mat[i][j] = complex(converted.re, converted.im)
+        this.#mat[i][j] = complex(converted.re, converted.im);
+        console.log(this.#mat[i][j]);
+        this.latexMat[i][j] = latex;
     }
 
     // apply_gate(gate_mat: ComplexMat2x2 ) {
@@ -61,18 +72,18 @@ export class DensityMatrix {
 
         console.log("apply gate")
         console.log("this.mat")
-        print_mat(this.mat);
+        print_mat(this.#mat);
         console.log("gate_mat")
         print_mat(gate_mat);
         let gate_dag = dagger(gate_mat)
-        this.mat = matmul(gate_mat, matmul(this.mat, gate_dag)) as ComplexMat2x2;
+        this.#mat = matmul(gate_mat, matmul(this.#mat, gate_dag)) as ComplexMat2x2;
     }
 
 
 
-    // get mat(){
-    //     return this.#mat;
-    // }
+    get mat(){
+        return this.#mat;
+    }
     // set mat(){
     //     return this.#mat;
     // }
@@ -86,16 +97,16 @@ export class DensityMatrix {
     }
 
     set a(value: Complex){
-        this.mat[0][0] = value;
+        this.#mat[0][0] = value;
     }
     set b(value: Complex){
-        this.mat[0][1] = value;
+        this.#mat[0][1] = value;
     }
     set c(value: Complex){
-        this.mat[1][0] = value;
+        this.#mat[1][0] = value;
     }
     set d(value: Complex){
-        this.mat[1][1] = value;
+        this.#mat[1][1] = value;
     }
 
     get a(){
