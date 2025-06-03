d368f429e595fecda4852403fdecb94db8afc1dd
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-06-03 23:08:42 +0200

    restructured code

--- bloch_app/src/lib/components/DynamicMatrix.svelte
+++ bloch_app/src/lib/components/DynamicMatrix.svelte
@@ -9,52 +9,35 @@ import { onMount } from 'svelte';
 	import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 	import { ComputeEngine } from '@cortex-js/compute-engine';
 
-
-    let ce = new ComputeEngine();
-    function setLatex(latex:string, DM: DensityMatrix, i: number, j:number) {
-        let converted = ce.parse(latex).N();
-        DM.mat[i][j] = complex(converted.re, converted.im)
-    }
-
-
-    function setLatexElements(node:MathfieldElement, latex: string[][]) {
-        if (!node){
-            return
-        }
-        for (let i = 0; i < 2; i++) {
-            for (let j = 0; j < 2; j++) {
-                console.log(`${latex[i][j]}`)
-                node.setPromptValue(`m${i}${j}`, latex[i][j], {})
-            }
-        }
-    }
-
-    let DM: DensityMatrix = getContext('densityMatrix')
-    setLatex('2i+\\pi', DM, 1,1)
+    let DM: DensityMatrix = getContext('densityMatrix');
 
     // dynMat.setLatexElements([['a', 'b'],['c', 'd']])
 	const myAttachment: Attachment = (element) => {
 		let mf = element as MathfieldElement;
         mf.value = '\\placeholder[mult]{1}\\cdot\\begin{bmatrix}\\placeholder[m00]{1} & \\placeholder[m01]{0}\\\\ \\placeholder[m10]{0} & \\placeholder[m11]{1}\\end{bmatrix}';
-        $inspect(DM.mat)
+        $inspect(DM)
 
         $effect(()=>{
             for (let i = 0; i < 2; i++){
                 for (let j = 0; j < 2; j++){
-                    mf.setPromptValue(`m${i}${j}`, DM.mat[i][j].toString(), {})
+                    let newValue: string = DM.latexMat[i][j] ?? DM.mat[i][j].toString();
+
+                    mf.setPromptValue(`m${i}${j}`, newValue, {})
                 }
             }
         })
 
         mf.addEventListener('change', ()=>{
-            let tempMat: ComplexMat2x2 = [[complex(), complex()],[complex(), complex()]];
+            // let tempMat: ComplexMat2x2 = [[complex(), complex()],[complex(), complex()]];
 
             for (let i = 0; i < 2; i++){
                 for (let j = 0; j < 2; j++){
-                    tempMat[i][j] = complex(mf.getPromptValue(`m${i}${j}`))
+                    // tempMat[i][j] = complex(mf.getPromptValue(`m${i}${j}`))
+                    let promptValue = mf.getPromptValue(`m${i}${j}`);
+                    DM.setLatex(promptValue, i, j)
                 }
             }
-            DM.mat = tempMat;
+            // DM.mat = tempMat;
         })
 		return () => {
 			console.log('cleaning up');
