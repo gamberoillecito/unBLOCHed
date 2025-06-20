25318ce8a6fbaf1a95ed8931e508dfb380a73b7a
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-06-20 22:14:40 +0200

    partially working path

--- bloch_app/src/lib/components/Path.svelte
+++ bloch_app/src/lib/components/Path.svelte
@@ -1,34 +1,51 @@
 <script lang="ts">
     import { T } from '@threlte/core'
-    import { MathUtils, Color, EllipseCurve, BufferGeometry, Group, Vector3, Line, AxesHelper, ArrowHelper } from 'three';
+    import { MathUtils, Color, EllipseCurve, BufferGeometry, Group, Vector3, Line, AxesHelper, ArrowHelper, Matrix4 } from 'three';
 	import type { GatePath } from './Model.svelte';
+	import { color } from 'three/src/nodes/TSL.js';
+	import { Matrix } from 'mathjs';
 
 	interface Props {
-		gatePath: GatePath;
+		path: GatePath;
 	}
 
 	let {
-        gatePath,
+        path,
     }: Props = $props();
 
     const curve = new EllipseCurve(
 	0,  0,            // ax, aY
 	1, 1,           // xRadius, yRadius
-	Math.PI/8,   Math.PI,  // aStartAngle, aEndAngle
+	0,   path.angle,  // aStartAngle, aEndAngle
 	false,            // aClockwise
 	0                 // aRotation
 );
+const Xaxis = new Vector3(1, 0, 0);
+const Yaxis = new Vector3(0, 1, 0);
+const Zaxis = new Vector3(0, 0, 1);
+const startingVector = new Vector3(...path.startingPoint);
 
 const points = curve.getPoints( 50 );
 const geometry = new BufferGeometry().setFromPoints( points )
-geometry.lookAt(new Vector3(...gatePath.axis))
-let ax = new Vector3(...gatePath.axis).normalize()
+
+const auxAxis = new Vector3().crossVectors(Xaxis, startingVector).normalize();
+const auxAngle = Math.acos(Xaxis.dot(startingVector) / (Xaxis.length() * startingVector.length()))
+const rotMatrix = new Matrix4().makeRotationAxis(auxAxis, auxAngle);
+
+geometry.applyMatrix4(rotMatrix);
+geometry.lookAt(new Vector3(...path.axis))
+
+const ah = new ArrowHelper((new Vector3(...path.axis)).normalize(), new Vector3(0,0,0), 1.4, '#ffffff', 0);
+// const ah = new ArrowHelper(Xaxis, new Vector3(0,0,0), 1.4, '#ffffff', 0);
+const originalBV = new ArrowHelper((new Vector3(...path.startingPoint)).normalize(), new Vector3(0,0,0), 1, '#FF0000');
 
 </script>
 
-<T.Line>
+<T.Line
+>
   <T is={geometry}/>
   <T.LineBasicMaterial color='red'/>
 </T.Line>
 
-<T.ArrowHelper args={[(new Vector3(1,1,1)).normalize()]} ></T.ArrowHelper>
+<T is={ah}></T>
+<T is={originalBV}></T>
