3709197f3187913dbf079eb4f2c097197e36c618
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-07-07 14:32:21 +0200

    installed shadcn

--- bloch_app/src/routes/+page.svelte
+++ bloch_app/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import App from '$lib/components/App.svelte'
 </script>
+<link
+ rel="stylesheet"
+ href="https://cdn.jsdelivr.net/npm/mathlive/mathlive-static.css"
+/>
+
 
 <div class="container">
   <div class="scene">
