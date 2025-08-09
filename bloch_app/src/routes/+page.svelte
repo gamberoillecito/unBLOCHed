8083b6a7b53b9651e70bdfad8bd669023c13a8f5
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-09 12:34:47 +0200

    feat: added loading animation

--- bloch_app/src/routes/+page.svelte
+++ bloch_app/src/routes/+page.svelte
@@ -2,29 +2,27 @@
 	import App from '$lib/components/App.svelte';
 	import CircleQuestionMark from '@lucide/svelte/icons/message-circle-question';
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
-	import { Toggle } from "$lib/components/ui/toggle/index.js";
+	import { Toggle } from '$lib/components/ui/toggle/index.js';
 	import Tutorial from '$lib/components/Tutorial.svelte';
-  import {onMount} from 'svelte'
-  let tutorialVisible = $state(false);
-  onMount(()=>{
-    window.scrollY = 300;
-    window.MathfieldElement.soundsDirectory = null;
-  })
-  
-  let innerWidth = $state(0);
-  
+	import { onMount } from 'svelte';
+	import LoaderCircle from '@lucide/svelte/icons/loader-circle';
+	let tutorialVisible = $state(false);
+	let loaded = $state(false);
+	onMount(() => {
+		window.scrollY = 300;
+		window.MathfieldElement.soundsDirectory = null;
+		loaded = true;
+	});
+
+	let innerWidth = $state(0);
+
 	let resizablePanelMin = $derived(Math.ceil((300 / innerWidth) * 100));
-  
 </script>
 
 <svelte:window bind:innerWidth />
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
-</style>
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mathlive/mathlive-static.css" />
-<div class="flex h-svh overflow-hidden flex-col ">
+<div class="flex h-svh flex-col overflow-hidden">
 	<!-- Header -->
 	<div
 		class="bg-foreground text-(--background) w-[100%] flex-none items-center justify-between p-2 text-center text-xl lg:flex lg:pl-6 lg:text-left lg:text-2xl"
@@ -34,28 +32,41 @@
 				class="font-normal">ed</span
 			> <span class="font-light">- Bloch sphere simulator</span></span
 		>
-    <Toggle variant="outline" bind:pressed={tutorialVisible} class="hidden lg:flex">
-
-      <CircleQuestionMark />
-      Tutorial
-    </Toggle>
+    {#if loaded}
+		<Toggle variant="outline" bind:pressed={tutorialVisible} class="hidden lg:flex">
+			<CircleQuestionMark />
+			Tutorial
+		</Toggle>
+    {/if}
 	</div>
 	<!-- Body -->
-	<Resizable.PaneGroup direction="horizontal">
-		<Resizable.Pane minSize={resizablePanelMin}>
-      <div class="@container h-full w-full flex-1 min-h-0">
-        <App />
-      </div>      
-    </Resizable.Pane>
-    {#if tutorialVisible}
-      <Resizable.Handle  withHandle/>
-      <Resizable.Pane minSize={resizablePanelMin}>
-      <div class="@container h-full w-full flex-1 min-h-0 p-2">
-        <Tutorial/>
-      </div>      
-      </Resizable.Pane>
-    {/if}
-	</Resizable.PaneGroup>
+	{#if !loaded}
+		<div
+			class="bg-muted flex h-full w-full animate-pulse flex-col place-content-center items-center"
+		>
+			<LoaderCircle class="h animate-spin" />
+			<p>Initializing qubit</p>
+		</div>
+	{:else}
+		<Resizable.PaneGroup direction="horizontal">
+			<Resizable.Pane minSize={resizablePanelMin}>
+				<div class="@container h-full min-h-0 w-full flex-1">
+					<App />
+				</div>
+			</Resizable.Pane>
+			{#if tutorialVisible}
+				<Resizable.Handle withHandle />
+				<Resizable.Pane minSize={resizablePanelMin}>
+					<div class="@container h-full min-h-0 w-full flex-1 p-2">
+						<Tutorial />
+					</div>
+				</Resizable.Pane>
+			{/if}
+		</Resizable.PaneGroup>
+	{/if}
 	<!-- Footer -->
 </div>
 
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+</style>
