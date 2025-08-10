d296565dc708f4abb6c3563b6ddbeb5a3d4ffed1
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-10 17:13:53 +0200

    fix: re-inserted CSS for katex (previously removed by mistake)

--- bloch_app/src/routes/+layout.svelte
+++ bloch_app/src/routes/+layout.svelte
@@ -12,4 +12,7 @@
 		class: 'w-fit flex flex-row border-1 gap-2 px-4 py-3  align-baseline shadow-sm rounded-md bg-card fg-card'
 	}}
 />
+
+<!-- CSS styles for katex to render formulas correctly -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css" integrity="sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn" crossorigin="anonymous">
 {@render children()}
