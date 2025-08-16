4b01eda6803ffaa8810f7945452c3b644f296c74
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-17 00:01:38 +0200

    feat: added joystickmode keyboard support and improved look

--- bloch_app/src/lib/components/custom-ui/JoystickControls.svelte
+++ bloch_app/src/lib/components/custom-ui/JoystickControls.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { FakeDensityMatrix } from "../Model.svelte";
+	import type { FakeDensityMatrix } from '../Model.svelte';
 	import { convertLatexToMarkup } from 'mathlive';
 	import Hand from '@lucide/svelte/icons/hand';
 	import MoveUp from '@lucide/svelte/icons/move-up';
@@ -9,53 +9,56 @@
 	import MoveDownRight from '@lucide/svelte/icons/move-down-right';
 	import MoveRight from '@lucide/svelte/icons/move-right';
 	import MoveLeft from '@lucide/svelte/icons/move-left';
-	import Mouse from '@lucide/svelte/icons/mouse'
+	import Mouse from '@lucide/svelte/icons/mouse';
 	import MoveDownLeft from '@lucide/svelte/icons/move-down-left';
-	import Label from "../ui/label/label.svelte";
+	import Keyboard from '@lucide/svelte/icons/keyboard';
+	import Label from '../ui/label/label.svelte';
+	import { onMount } from 'svelte';
+	import Separator from '../ui/separator/separator.svelte';
+	interface Props {
+		DM: FakeDensityMatrix;
+		joystickMode: boolean;
+	}
 
-    interface Props {
-        DM: FakeDensityMatrix;
-    }
+	let { DM, joystickMode = $bindable() }: Props = $props();
 
-    let {
-        DM,
-    }: Props = $props();
-
-    let isDragging = $state(false);
+	$inspect(joystickMode);
+	let isDragging = $state(false);
 	let lastX = $state(0);
 	let lastY = $state(0);
+	const avgEventsOld = 40;
+	let oldCounter = 0;
 
 	function handleMouseDown(e: MouseEvent) {
-	    isDragging = true;
-	    lastX = e.clientX;
-	    lastY = e.clientY;
-	    // Listen globally for mouseup and mousemove
-	    window.addEventListener('mousemove', handleMouseMove);
-	    window.addEventListener('mouseup', handleMouseUp);
+		isDragging = true;
+		lastX = e.clientX;
+		lastY = e.clientY;
+		// Listen globally for mouseup and mousemove
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseup', handleMouseUp);
 	}
 
 	function handleMouseUp() {
-	    isDragging = false;
-	    // Remove global listeners
-	    window.removeEventListener('mousemove', handleMouseMove);
-	    window.removeEventListener('mouseup', handleMouseUp);
+		isDragging = false;
+		// Remove global listeners
+		window.removeEventListener('mousemove', handleMouseMove);
+		window.removeEventListener('mouseup', handleMouseUp);
 	}
 
 	function handleMouseMove(e: MouseEvent) {
-	    if (!isDragging) return;
-	    const dx = e.clientX - lastX;
-	    const dy = e.clientY - lastY;
-		
+		if (!isDragging) return;
+		const dx = e.clientX - lastX;
+		const dy = e.clientY - lastY;
 
-	    // Sensitivity factors (adjust as needed)
-	    const thetaStep = 0.01;
-	    const phiStep = 0.01;
+		// Sensitivity factors (adjust as needed)
+		const thetaStep = 0.01;
+		const phiStep = 0.01;
 
-	    DM.theta = DM.theta - dy * thetaStep;
-	    DM.phi = (DM.phi + dx * phiStep) % (2 * Math.PI);
+		DM.theta = DM.theta - dy * thetaStep;
+		DM.phi = (DM.phi + dx * phiStep) % (2 * Math.PI);
 
-	    lastX = e.clientX;
-	    lastY = e.clientY;
+		lastX = e.clientX;
+		lastY = e.clientY;
 	}
 
 	function handleScroll(e: WheelEvent) {
@@ -63,58 +66,112 @@
 		DM.length += -Math.sign(e.deltaY) * lengthStep;
 	}
 
+	function handleKeys(e: KeyboardEvent) {
+		if (!joystickMode) return;
+
+		console.log(e.key);
+		const thetaStep = 0.1;
+		const phiStep = 0.1;
+		const lenStep = 0.1;
+
+		switch (e.key) {
+			case 'w':
+			case 'ArrowUp':
+				DM.theta += thetaStep;
+				break;
+			case 's':
+			case 'ArrowDown':
+				DM.theta -= thetaStep;
+				break;
+			case 'a':
+			case 'ArrowLeft':
+				DM.phi -= phiStep;
+				break;
+			case 'd':
+			case 'ArrowRight':
+				DM.phi += phiStep;
+				break;
+			case 'm':
+			case '-':
+				DM.length -= lenStep;
+				break;
+			case 'p':
+			case '=':
+			case '+':
+				DM.length += lenStep;
+				break;
+			default:
+				break;
+		}
+	}
+
 	$effect(() => {
 		const btn = document.getElementById('joystick-btn');
 		if (isDragging) {
-			document.body.style.cursor = "grabbing";
-			if (btn) btn.style.cursor = "grabbing";
+			document.body.style.cursor = 'grabbing';
+			if (btn) btn.style.cursor = 'grabbing';
 		} else {
-			document.body.style.cursor = "";
-			if (btn) btn.style.cursor = "grab";
+			document.body.style.cursor = '';
+			if (btn) btn.style.cursor = 'grab';
 		}
 	});
+	document.addEventListener('keydown', handleKeys);
 </script>
 
 <div class="flex flex-col items-center gap-2">
-<button
+	<button
+		id="joystick-btn"
+		class="grid w-[300px] aspect-square @lg:h-[200px] @lg:w-[200px] grid-cols-3 grid-rows-3 place-content-center place-items-center items-center rounded-[1em] border-2"
+		onmousedown={handleMouseDown}
+		onwheel={handleScroll}
+		aria-label="sphere-controls"
+	>
+		<MoveUpLeft class="opacity-40" />
+		<div>
+			<MoveUp />
+			<p class="pointer-events-none">{@html convertLatexToMarkup('\\mathbf{\\theta -}')}</p>
+		</div>
+		<MoveUpRight class="opacity-40" />
+		<div class="grid grid-cols-2 gap-2">
+			<MoveLeft />
+			<p class="pointer-events-none">{@html convertLatexToMarkup('\\mathbf{\\varphi -}')}</p>
+		</div>
+		<div></div>
+		<div class="grid grid-cols-2 gap-2">
+			<p class="pointer-events-none">{@html convertLatexToMarkup('\\mathbf{\\varphi +}')}</p>
+			<MoveRight />
+		</div>
+		<MoveDownLeft class="opacity-40" />
+		<div>
+			<p class="pointer-events-none">{@html convertLatexToMarkup('\\mathbf{\\theta +}')}</p>
+			<MoveDown />
+		</div>
+		<MoveDownRight class="opacity-40" />
+	</button>
 
-	id="joystick-btn"
-	class="w-[200px] h-[200px] border-2 rounded-[1em] grid grid-cols-3 grid-rows-3 items-center place-content-center place-items-center"
-	onmousedown={handleMouseDown}
-	onwheel={handleScroll}
-	aria-label="sphere-controls"
->
-<MoveUpLeft class="opacity-40"/>
-<div>
-	<MoveUp/>
-	<p class="pointer-events-none"> {@html convertLatexToMarkup("\\mathbf{\\theta -}")}</p>
-</div>
-<MoveUpRight class="opacity-40"/>
-<div class="grid grid-cols-2 gap-2">
-	<MoveLeft/>
-	<p class="pointer-events-none"> {@html convertLatexToMarkup("\\mathbf{\\varphi -}")}</p>
-</div>
-<div >
-</div>
-<div class="grid grid-cols-2 gap-2">
-	<p class="pointer-events-none"> {@html convertLatexToMarkup("\\mathbf{\\varphi +}")}</p>
-	<MoveRight/>
-</div>
-<MoveDownLeft class="opacity-40"/>
-<div>
-	<p class="pointer-events-none"> {@html convertLatexToMarkup("\\mathbf{\\theta +}")}</p>
-	<MoveDown/>
-</div>
-<MoveDownRight class="opacity-40"/>
-</button>
-<Label for="joystick-button" > 
-	<div class="flex flex-col gap-2">
-		<p class="flex flex-row items-center gap-1">
-			<Mouse/> Scroll: length 
-		</p>
-		<p class="flex flex-row items-center gap-1">
-		<Hand/> Drag: angles
-		</p>
+	<div class="@lg:flex hidden flex-col">
+		<Label for="joystick-button">
+			<div class="flex flex-col gap-2">
+				<p class="flex flex-row items-center gap-1">
+					<Mouse /> Scroll: length
+				</p>
+				<p class="flex flex-row items-center gap-1">
+					<Hand /> Drag: angles
+				</p>
+
+				<p class="flex flex-row items-center gap-1"></p>
+			</div>
+		</Label>
+		<Separator />
+		<div class="flex-cols flex gap-2">
+			<Keyboard />
+			<div class="grid">
+				<p>WASD / Arrows</p>
+			</div>
+		</div>
 	</div>
-	</Label>
 </div>
+<!-- <div class="grid grid-cols-3 grid-rows-2 justify-center">
+	<MoveUp class="col-start-2 col-span-2 place-content-center items-center justify-center keyboard-key"/>
+	<MoveLeft class="keyboard-key"/> <MoveDown class="translate-y-[0.2rem]"/> <MoveRight class="keyboard-key"/>
+</div> -->
