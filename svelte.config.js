2a9df31f05e37d706827be88807dad80fa451620
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-21 09:57:39 +0200

    fix: changed base path

--- svelte.config.js
+++ svelte.config.js
@@ -32,7 +32,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/unBLOCHed'
 		}
 	},
 	extensions: ['.svelte', '.svx']
