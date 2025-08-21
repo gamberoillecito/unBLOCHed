0c37d659a5efaf0054231c238fbf6afe13be3509
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-21 14:53:11 +0200

    fix: attempt fixing static files, restored paths.base to the value on svelte docs

--- svelte.config.js
+++ svelte.config.js
@@ -31,7 +31,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/unBLOCHed'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	extensions: ['.svelte', '.svx']
