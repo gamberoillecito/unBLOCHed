de67966b64616954b3916f5dd442483bb19e15b4
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-25 09:36:28 +0200

    feat: draft attempt at implementing support notification

---
+++ src/lib/components/SupportNotification.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import { onMount, type Component } from 'svelte';
+	import { toast } from 'svelte-sonner';
+    import GitHubIcon from './custom-ui/GitHubIcon.svelte';
+    console.log(typeof(GitHubIcon));
+    
+	onMount(() => {
+		toast('Do you like this website?', {
+			description: "If you do, star it on GitHub. If you don't, let me know how to improve it!",
+            action: {
+                label: GitHubIcon as Component,
+                onClick: ()=> console.log('Ciao')
+            },
+            duration: 99999
+		});
+	});
+</script>
