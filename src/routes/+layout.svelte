9a5a8cdb04e91965a0a0767d93f2b2fbabe54888
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-08-26 16:01:32 +0200

    feat: suboptimal implementation of toasts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
-
 <script lang="ts">
 	import '../app.css';
-	import { Toaster } from "$lib/components/ui/sonner";
-	import { ModeWatcher } from "mode-watcher";
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Button, buttonVariants, type ButtonVariant } from '$lib/components/ui/button/index.js';
 	let { children } = $props();
-
 </script>
+
 <ModeWatcher />
 <Toaster
 	toastOptions={{
 		unstyled: true,
-		class: 'w-fit flex flex-row border-1 gap-2 px-4 py-3  align-baseline shadow-sm rounded-md bg-card fg-card'
+		classes: {
+			toast:
+				'flex flex-row border-1 gap-2 px-4 py-3 items-stretch justify-items-start shadow-sm rounded-md bg-card fg-card w-fit min-w-[300px] max-w-[500px] justify-between',
+			title: 'prose dark:prose-invert grow m-t-0',
+			description: 'prose-sm grow',
+			actionButton: `${buttonVariants.variants.variant.outline} size-fit p-1 col-span-1 translate-y-2 w-fit`,
+			icon: 'size-4 col-span-1 shrink self-start translate-y-2'
+		}
 	}}
 />
 
@@ -18,24 +25,27 @@
 <svelte:head>
 	<title>unBLOCHed</title>
 	<meta name="description" content="Interactive and intuitive Bloch spehere simulator" />
-	<script defer src="https://cloud.umami.is/script.js" data-website-id="0ee63b18-31fe-4396-9051-84766149f498"></script>
+	<script
+		defer
+		src="https://cloud.umami.is/script.js"
+		data-website-id="0ee63b18-31fe-4396-9051-84766149f498"
+	></script>
 </svelte:head>
 {@render children()}
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400..700&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400..700&display=swap');
 
-@import url('https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css');
-:global(math-field::part(menu-toggle)) {
-  display: none;
-}
-:global(math-field::part(virtual-keyboard-toggle)) {
-    display: none;
-}
-:global(math-field) {
-    /* background-color: var(--color-background); */
-	background-color: inherit;
-	color: var(--color-foreground);
-}
-
+	@import url('https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css');
+	:global(math-field::part(menu-toggle)) {
+		display: none;
+	}
+	:global(math-field::part(virtual-keyboard-toggle)) {
+		display: none;
+	}
+	:global(math-field) {
+		/* background-color: var(--color-background); */
+		background-color: inherit;
+		color: var(--color-foreground);
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
 	import { Toggle } from '$lib/components/ui/toggle/index.js';
 	import Tutorial from '$lib/components/Tutorial.svelte';
-	import { onMount } from 'svelte';
+	import { createRawSnippet, onMount } from 'svelte';
 	import LoaderCircle from '@lucide/svelte/icons/loader-circle';
 	import 'mathlive/static.css';
 	import SunIcon from '@lucide/svelte/icons/sun';
@@ -13,14 +13,16 @@
 	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
 	import Title from '$lib/components/Title.svelte';
 	import { MediaQuery } from 'svelte/reactivity';
-	import Info from '@lucide/svelte/icons/info'
+	import Info from '@lucide/svelte/icons/info';
 	import Welcome from '$lib/components/Welcome.svelte';
 	import { preferences } from '$lib/preferences';
 	import { get } from 'svelte/store';
 	import { toast } from 'svelte-sonner';
-	import SupportNotification from '$lib/components/SupportNotification.svelte'
-	
-	let showWelcomeAtStart= get(preferences).showWelcomeAtStart;	
+	import SupportNotification from '$lib/components/SupportNotification.svelte';
+	import CustomToast from '$lib/components/custom-ui/CustomToast.svelte';
+	import GitHubIcon from '$lib/components/custom-ui/GitHubIcon.svelte';
+
+	let showWelcomeAtStart = get(preferences).showWelcomeAtStart;
 	let tutorialVisible = $state(false);
 	let loaded = $state(false);
 
@@ -41,50 +43,74 @@
 		if (showWelcomeAtStart) {
 			welcomeMessageOpen = true;
 		}
-		toast(prova(), {duration: 99999});
+
+		toast('Do you like unBLOCHed?', {
+			icon: GitHubIcon,
+			action: {
+				label: 'GitHub',
+				onClick: () => {}
+			},
+			description: 'Give it a star!',
+			duration: 99999
+		});
+		toast('Do you like unBLOCHed?', {
+			icon: GitHubIcon,
+			action: {
+				label: 'GitHub',
+				onClick: () => {}
+			},
+			description: 'Give it a star!',
+			duration: 99999
+		});
 	});
 
-	const isDesktop = new MediaQuery("(min-width: 768px)");
-
+	const isDesktop = new MediaQuery('(min-width: 768px)');
 </script>
 
-{#snippet prova()}
-	<Title/>
-{/snippet}
 <svelte:window bind:innerWidth />
 
-<Welcome bind:open={welcomeMessageOpen}/>
+<Welcome bind:open={welcomeMessageOpen} />
 
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mathlive/mathlive-static.css" /> -->
 <div class="flex h-svh flex-col overflow-hidden">
 	<!-- Header -->
 	<div
-		class="bg-foreground text-(--background) w-[100%]  flex-row-reverse lg:flex-row items-center justify-between p-2 text-center text-xl flex px-6 lg:text-left lg:text-2xl"
+		class="bg-foreground flex w-[100%] flex-row-reverse items-center justify-between p-2 px-6 text-center text-xl text-(--background) lg:flex-row lg:text-left lg:text-2xl"
 	>
 		<Button onclick={toggleMode} variant="secondary" size="sm" class="group scale-80 md:scale-100">
 			<MoonIcon
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all delay-300 duration-500 dark:-rotate-180 dark:scale-0"
+				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all delay-300 duration-500 dark:scale-0 dark:-rotate-180"
 			/>
 			<SunIcon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-180 scale-0 !transition-all delay-300 duration-500 dark:rotate-0 dark:scale-100"
+				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-180 !transition-all delay-300 duration-500 dark:scale-100 dark:rotate-0"
 			/>
 			<span class="sr-only">Toggle theme</span>
 		</Button>
 		<div>
 			<!-- The line with title is a hacky way to change the title style using CSS, 
 			if using svelte media queries it gets calculated only after part of the content has loaded -->
-			<Title subtitle={false}/> <span class="hidden lg:inline"><Title title={false}/></span>
+			<Title subtitle={false} /> <span class="hidden lg:inline"><Title title={false} /></span>
 			<!-- <Toggle bind:pressed={welcomeMessageOpen} class={buttonVariants.variants.variant.link}>
 				<Info />
 			</Toggle> -->
-			<Button variant="ghost" onclick={()=>{welcomeMessageOpen = !welcomeMessageOpen}} aria-label="info about website">
+			<Button
+				variant="ghost"
+				onclick={() => {
+					welcomeMessageOpen = !welcomeMessageOpen;
+				}}
+				aria-label="info about website"
+			>
 				<Info />
 			</Button>
 		</div>
-			<Toggle variant="outline" bind:pressed={tutorialVisible} class="hidden lg:flex {loaded? '': 'opacity-0'}">
-				<CircleQuestionMark />
-				Tutorial
-			</Toggle>
+		<Toggle
+			variant="outline"
+			bind:pressed={tutorialVisible}
+			class="hidden lg:flex {loaded ? '' : 'opacity-0'}"
+		>
+			<CircleQuestionMark />
+			Tutorial
+		</Toggle>
 	</div>
 	<!-- Body -->
 	{#if !loaded}
@@ -99,14 +125,14 @@
 	{:else}
 		<Resizable.PaneGroup direction="horizontal">
 			<Resizable.Pane minSize={resizablePanelMin}>
-				<div class="@container bg-background h-full min-h-0 w-full flex-1">
+				<div class="bg-background @container h-full min-h-0 w-full flex-1">
 					<App />
 				</div>
 			</Resizable.Pane>
 			{#if tutorialVisible}
 				<Resizable.Handle withHandle />
 				<Resizable.Pane minSize={resizablePanelMin}>
-					<div class="@container bg-background h-full min-h-0 w-full flex-1 p-2">
+					<div class="bg-background @container h-full min-h-0 w-full flex-1 p-2">
 						<Tutorial />
 					</div>
 				</Resizable.Pane>
