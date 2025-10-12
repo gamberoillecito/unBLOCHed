5b2638c10d3f87f50843330844c8acac24c767ab
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-10-12 10:04:07 +0200

    feat: custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<div class="fixed inset-0 flex items-center justify-center p-4">
+	<article class=" prose dark:prose-invert max-w-[min(80ch,90%)] mx-5 border-2 p-6 text-center text-xl md:text-2xl text-warning">
+		There's nothing <em>quantum</em> here, only the <em>classic</em> 404 error
+		<br />
+	</article>
+</div>
