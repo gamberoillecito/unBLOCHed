d4620d4235c544d5b742d6654f599fd979b3e6a7
Giacomo Bertelli <itjustfordrive@gmail.com>
2026-03-21 16:24:51 +0100

    chore: testing precompress

--- svelte.config.js
+++ svelte.config.js
@@ -27,7 +27,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			fallback: '404.html',
-			precompress: false,
+			precompress: true,
 			strict: true
 		}),
 		paths: {
